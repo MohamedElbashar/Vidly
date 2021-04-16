cbcd04d5a6082c04ac5ed1fd49d6b5d957f9e689
Mohamed Elbashar <mohamedbashar200@gmail.com>
2021-04-16 18:10:48 +0200

    build user api

---
+++ routes/users.js
@@ -0,0 +1,13 @@
+/** @format */
+
+const { User, validate } = require("../models/user");
+const mongoose = require("mongoose");
+const express = require("express");
+const router = express.Router();
+
+router.post("/", async (req, res) => {
+  const users = await User.find().sort("name");
+  res.send(users);
+});
+
+module.exports = router;
