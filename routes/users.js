926b5b6e21390b01ae0af4fcb9ecf0e4df4a85e8
Mohamed Elbashar <mohamedbashar200@gmail.com>
2021-04-17 17:28:37 +0200

    Getting the Current User

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
 /** @format */
+const auth = require("../middleware/auth");
 const jwt = require("jsonwebtoken");
 const config = require("config");
 const bcrypt = require("bcrypt");
@@ -8,6 +9,10 @@ const mongoose = require("mongoose");
 const express = require("express");
 const router = express.Router();
 
+router.get("/me", auth, async (req, res) => {
+  const user = await User.findById(req.user._id).select("-password");
+  res.send(user);
+});
 router.post("/", async (req, res) => {
   const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
