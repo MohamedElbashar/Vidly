81815d6f1c9f09119437e1ea63125bd0e00a2d42
Mohamed Elbashar <mohamedbashar200@gmail.com>
2021-04-18 02:16:07 +0200

    Removing Try , catch

---
+++ middleware/async.js
@@ -0,0 +1,11 @@
+/** @format */
+
+module.exports = function asyncMiddleWare(handler) {
+  return async (req, res, next) => {
+    try {
+      await handler(req, res);
+    } catch (ex) {
+      next(ex);
+    }
+  };
+};
