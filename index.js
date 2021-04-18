e9558451703ccec05f3ff4422dbda2bc712bf9d5
Mohamed Elbashar <mohamedbashar200@gmail.com>
2021-04-18 16:20:56 +0200

    extraction Routes and database in index.js

--- index.js
+++ index.js
@@ -1,60 +1,33 @@
 /** @format */
 require("express-async-errors");
 require("winston-mongodb");
-const error = require("./middleware/error");
+
 const winston = require("winston");
 const config = require("config");
 const Joi = require("joi");
 Joi.objectId = require("joi-objectid")(Joi);
-const mongoose = require("mongoose");
-
-const genres = require("./routes/genres");
-const customers = require("./routes/customers");
-const movies = require("./routes/movies");
-const rentals = require("./routes/rentals");
-const users = require("./routes/users");
-const auth = require("./routes/auth");
 
 const express = require("express");
 const app = express();
 
-process.on("uncaughtException", (ex) => {
-  winston.error(ex.message, ex);
-  process.exit(1);
-});
+require("./startup/routes")(app);
+require("./startup/db")();
 
 winston.handleExceptions(
   new winston.transports.File({ filename: "uncaughtException.log" })
 );
 
 process.on("unHandledRejection", (ex) => {
-  winston.error(ex.message, ex);
-  process.exit(1);
+  throw ex;
 });
+
 winston.add(winston.transports.File, { filename: "logfile.log" });
 winston.add(winston.transports.mongoDB, { db: "mongodb://localhost/vidly" });
-
-const p = Promise.reject(new Error("something failed miserably"));
-p.then(() => console.log("Done"));
 
 if (!config.get("jwtPrivateKey")) {
   console.error("FATAL ERROR:jwtPrivateKey is not defined");
   process.exit(1);
 }
-
-mongoose
-  .connect("mongodb://localhost/vidly")
-  .then(() => console.log("Connected to MongoDB..."))
-  .catch((err) => console.error("Could not connect to MongoDB..."));
-
-app.use(express.json());
-app.use("/api/genres", genres);
-app.use("/api/customers", customers);
-app.use("/api/movies", movies);
-app.use("/api/rentals", rentals);
-app.use("/api/users", users);
-app.use("/api/auth", auth);
-app.use(error);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
