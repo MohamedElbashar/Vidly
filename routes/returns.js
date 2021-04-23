f12c0653db1993245a61fee442e308a5ff173e62
Mohamed Elbashar <mohamedbashar200@gmail.com>
2021-04-23 17:42:04 +0200

    Testing if Rental Processed

--- routes/returns.js
+++ routes/returns.js
@@ -1,5 +1,5 @@
 /** @format */
-
+const { Rentals } = require("../models/rental");
 const express = require("express");
 const router = express.Router();
 
@@ -9,6 +9,16 @@ router.post("/", async (req, res) => {
   }
   if (!req.body.movieId) {
     return res.status(400).send("movieId not provided");
+  }
+  const rental = await Rentals.findOne({
+    "customer._id": req.body.customerId,
+    "movie._id": req.body.movieId,
+  });
+  if (!rental) {
+    return res.status(404).send("Rental not found");
+  }
+  if (rental.dateReturned) {
+    return res.status(400).send("return already processed");
   }
   res.status(401).send("Unauthorized");
 });
--- tests/integration/returns.test.js
+++ tests/integration/returns.test.js
@@ -54,4 +54,17 @@ describe("/api/returns", () => {
     const res = await exec();
     expect(res.status).toBe(400);
   });
+  it("should return 404 if no rental found for customer/movie", async () => {
+    await Rentals.remove({});
+
+    const res = await exec();
+    expect(res.status).toBe(404);
+  });
+  it("should return 400 if return is already processed", async () => {
+    rental.dateReturned = new Date();
+    await rental.save();
+
+    const res = await exec();
+    expect(res.status).toBe(400);
+  });
 });
